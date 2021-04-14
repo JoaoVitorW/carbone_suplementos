2979180ec41be43600283c26dcc5a2cf53580166
Joao Vitor <jvwc14@gmail.com>
2021-04-14 15:28:36 -0300

    pag1 melhorada pag 2 criada

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function pag1(){
+    window.location.href="http://127.0.0.1:5500/index.html#"
+}
+
+function pag2(){
+    window.location.href="http://127.0.0.1:5500/produtos2.html"
+}
+
+function home(){
+    window.location.href="http://127.0.0.1:5500/index.html#"
+}
